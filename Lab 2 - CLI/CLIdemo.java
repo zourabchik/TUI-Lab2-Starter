e26133077a3741efa56799065723d04ed8cff6c6
Alexander Babich <alexander.taurus@gmail.com>
2020-05-15 21:02:47 +0300

    Add files via upload

---
+++ Lab 2 - CLI/CLIdemo.java
@@ -0,0 +1,155 @@
+package com.mybank.tui;
+
+import com.mybank.domain.Bank;
+import com.mybank.domain.CheckingAccount;
+import com.mybank.domain.Customer;
+import com.mybank.domain.SavingsAccount;
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.jline.reader.*;
+import org.jline.reader.impl.completer.*;
+import org.jline.utils.*;
+import org.fusesource.jansi.*;
+
+/**
+ * Sample application to show how jLine can be used.
+ *
+ * @author sandarenu
+ *
+ */
+/**
+ * Console client for 'Banking' example
+ *
+ * @author Alexander 'Taurus' Babich
+ */
+public class CLIdemo {
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    private String[] commandsList;
+
+    public void init() {
+        commandsList = new String[]{"help", "customers", "customer", "exit"};
+    }
+
+    public void run() {
+        AnsiConsole.systemInstall(); // needed to support ansi on Windows cmd
+        printWelcomeMessage();
+        LineReaderBuilder readerBuilder = LineReaderBuilder.builder();
+        List<Completer> completors = new LinkedList<Completer>();
+
+        completors.add(new StringsCompleter(commandsList));
+        readerBuilder.completer(new ArgumentCompleter(completors));
+
+        LineReader reader = readerBuilder.build();
+
+        String line;
+        PrintWriter out = new PrintWriter(System.out);
+
+        while ((line = readLine(reader, "")) != null) {
+            if ("help".equals(line)) {
+                printHelp();
+            } else if ("customers".equals(line)) {
+                AttributedStringBuilder a = new AttributedStringBuilder()
+                        .append("\nThis is all of your ")
+                        .append("customers", AttributedStyle.BOLD.foreground(AttributedStyle.RED))
+                        .append(":");
+
+                System.out.println(a.toAnsi());
+                if (Bank.getNumberOfCustomers() > 0) {
+                    System.out.println("\nLast name\tFirst Name\tBalance");
+                    System.out.println("---------------------------------------");
+                    for (int i = 0; i < Bank.getNumberOfCustomers(); i++) {
+                        System.out.println(Bank.getCustomer(i).getLastName() + "\t\t" + Bank.getCustomer(i).getFirstName() + "\t\t$" + Bank.getCustomer(i).getAccount(0).getBalance());
+                    }
+                } else {
+                    System.out.println(ANSI_RED+"Your bank has no customers!"+ANSI_RESET);
+                }
+
+            } else if (line.indexOf("customer") != -1) {
+                try {
+                    int custNo = 0;
+                    if (line.length() > 8) {
+                        String strNum = line.split(" ")[1];
+                        if (strNum != null) {
+                            custNo = Integer.parseInt(strNum);
+                        }
+                    }                    
+                    Customer cust = Bank.getCustomer(custNo);
+                    String accType = cust.getAccount(0) instanceof CheckingAccount ? "Checkinh" : "Savings";
+                    
+                    AttributedStringBuilder a = new AttributedStringBuilder()
+                            .append("\nThis is detailed information about customer #")
+                            .append(Integer.toString(custNo), AttributedStyle.BOLD.foreground(AttributedStyle.RED))
+                            .append("!");
+
+                    System.out.println(a.toAnsi());
+                    
+                    System.out.println("\nLast name\tFirst Name\tAccount Type\tBalance");
+                    System.out.println("-------------------------------------------------------");
+                    System.out.println(cust.getLastName() + "\t\t" + cust.getFirstName() + "\t\t" + accType + "\t$" + cust.getAccount(0).getBalance());
+                } catch (Exception e) {
+                    System.out
+                        .println(ANSI_RED + "ERROR! Wrong customer number!" + ANSI_RESET);
+                }
+            } else if ("exit".equals(line)) {
+                System.out.println("Exiting application");
+                return;
+            } else {
+                System.out
+                        .println(ANSI_RED + "Invalid command, For assistance press TAB or type \"help\" then hit ENTER." + ANSI_RESET);
+            }
+        }
+
+        AnsiConsole.systemUninstall();
+    }
+
+    private void printWelcomeMessage() {
+        System.out
+                .println("\nWelcome to " + ANSI_GREEN + " MyBank Console Client App" + ANSI_RESET + "! \nFor assistance press TAB or type \"help\" then hit ENTER.");
+
+    }
+
+    private void printHelp() {
+        System.out.println("help\t\t\t- Show help");
+        System.out.println("customer\t\t- Show list of customers");
+        System.out.println("customer \'index\'\t- Show customer details");
+        System.out.println("exit\t\t\t- Exit the app");
+
+    }
+
+    private String readLine(LineReader reader, String promtMessage) {
+        try {
+            String line = reader.readLine(promtMessage + ANSI_YELLOW + "\nbank> " + ANSI_RESET);
+            return line.trim();
+        } catch (UserInterruptException e) {
+            // e.g. ^C
+            return null;
+        } catch (EndOfFileException e) {
+            // e.g. ^D
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Bank.addCustomer("John", "Doe");
+        Bank.addCustomer("Fox", "Mulder");
+        Bank.getCustomer(0).addAccount(new CheckingAccount(2000));
+        Bank.getCustomer(1).addAccount(new SavingsAccount(1000, 3));
+
+        CLIdemo shell = new CLIdemo();
+        shell.init();
+        shell.run();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# TUI-Lab2-Starter
+# UI Labs
+![](terminal-icon.png)
+![](gui-icon.png)
+
+Ці роботи доповнюють основний цикл лабораторних робіт #1-8 (проект **Banking**, [Netbeans](https://netbeans.org/)) з ООП на третьому курсі [ППК НТУ "ХПІ](http://polytechnic.poltava.ua)". Основна мета цих додаткових вправ - познайомитись з різними видами інтерфейсів користувача та засобами їх створення. Згадувані 'базові' роботи розміщено в [окремому репозиторії](https://github.com/liketaurus/OOP-JAVA).
+
+На даний момент цей репозиторій містить **наступні роботи**:
+- **Текстові інтерфейси** (TUI, CLI):
+  - робота 1 - [TUI з Jexer](https://github.com/liketaurus/TUI-Labs/blob/master/Lab%201%20-TUI/Lab%201.md)
+  - робота 2 - [CLI з jline 3](https://github.com/liketaurus/TUI-Labs/blob/master/Lab%202%20-%20CLI/Lab%202.md)
+- **Swing** GUI:
+  - робота 3 - [GUI з Swing](https://github.com/liketaurus/TUI-Labs/blob/master/Lab%203%20-%20SWING/Lab%203.md)
+  - робота 4 - [GUI з Matisse](https://github.com/liketaurus/TUI-Labs/blob/master/Lab%204%20-%20Matisse/Lab%204.md)
+- **JavaFX** GUI:
+  - робота 5 - [GUI з JavaFX](https://github.com/liketaurus/TUI-Labs/blob/master/Lab%205%20-%20JavaFX/Lab%205.md)
+  - робота 6 - [GUI з Monet](https://github.com/liketaurus/TUI-Labs/blob/master/Lab%206%20-%20Monet/Lab%206.md)
+- **Netbeans Platform**
+  - робота 7 - [GUI з Netbeans Platform](https://github.com/liketaurus/TUI-Labs/blob/master/Lab%207%20-%20Netbeans%20Platform/Lab%207.md)
+
+**Додаткові завдання** (для тих хто зробив все і прагне більшого):
+- [для роботи 1](https://github.com/liketaurus/TUI-Labs/blob/master/Lab%201%20-TUI/Lab%201%20-%20add.md)
+- [для роботи 2](https://github.com/liketaurus/TUI-Labs/blob/master/Lab%202%20-%20CLI/Lab%202%20-%20add.md)
+- [для роботи 3](https://github.com/liketaurus/TUI-Labs/blob/master/Lab%203%20-%20SWING/Lab%203%20-%20add.md)
+- [для роботи 4](https://github.com/liketaurus/TUI-Labs/blob/master/Lab%204%20-%20Matisse/Lab%20-%204%20-%20add.md)
+- [для роботи 5](https://github.com/liketaurus/TUI-Labs/blob/master/Lab%205%20-%20JavaFX/Lab%20-%205%20-%20add.md)
+- [для роботи 6](https://github.com/liketaurus/TUI-Labs/blob/master/Lab%206%20-%20Monet/Lab%206%20-%20add.md)
+- [для роботи 7](https://github.com/liketaurus/TUI-Labs/blob/master/Lab%207%20-%20Netbeans%20Platform/Lab%20-7%20-%20add.md)
+
+Всі необхідні бібліотеки містяться у теці [jars](https://github.com/liketaurus/TUI-Labs/tree/master/jars). В тому числі - всі необхідні класи з робіт 1-8 - файл [MyBank.jar](https://github.com/liketaurus/TUI-Labs/blob/master/jars/MyBank.jar). Файл даних лежить у теці [data](https://github.com/liketaurus/TUI-Labs/tree/master/data).
+
+---
+**УВАГА! Не забувайте завантажувати результати виконання робіт до своїх репозиторіїв!**
+
+
+![](https://img.shields.io/badge/Made%20with-JAVA-red.svg)
+![](https://img.shields.io/badge/Made%20with-%20Netbeans-brightgreen.svg)
+![](https://img.shields.io/badge/Made%20at-PPC%20NTU%20%22KhPI%22-blue.svg) 
